092f51b29f7f68e75042defd8185288aae91309e
wu-chinese <merkurieffr@gmail.com>
2023-12-03 00:10:02 -0800

    completed day 2 part 1

--- day_two/day_two.py
+++ day_two/day_two.py
@@ -1,12 +1,54 @@
 # given 12 red, 13 gree, 14 blue
 
 
+def gambling(line: str) -> bool:
+
+
+  # parse for blue red or green
+
+  # take the second part without the game
+  curr_line = line.split(':')[1]
+
+  # parse the inputs
+  curr_game = curr_line.split(';')
+
+  for game in curr_game:
+    red = 0
+    green = 0
+    blue = 0
+    for curr in game.split(","):
+      curr = curr.strip()
+      count, color = curr.split(" ")
+      count = int(count)
+
+      print(count, " ", color)
+
+      if color[0] == 'r':
+        red += count
+
+      if color[0] == 'g':
+        green += count
+
+      if color[0] == 'b':
+        blue += count
+
+      if red > 12 or green > 13 or blue > 14:
+        return False
+
+  return True
+
 
 
 def main():
+  ret = 0
+  count = 0
   with open('input.txt', 'r') as f:
     for line in f:
-      ret += int(parse_this_shit(line))
+      count += 1
+      if gambling(line) == True:
+        ret += count
+
+  print(ret)
 
 if __name__ == "__main__":
   main()
